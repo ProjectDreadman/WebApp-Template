269ecfe55039149e0cd6b4ab21c686a55a56e76f
ProjectDreadman <168795340+ProjectDreadman@users.noreply.github.com>
2024-06-15 22:35:56 -0400

    Create App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './styles.css';
+import Home from './pages/Home';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to Your Web App</h1>
+      </header>
+      <main>
+        <Home />
+      </main>
+    </div>
+  );
+}
+
+export default App;
